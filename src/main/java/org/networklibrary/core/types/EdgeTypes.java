527ec2e9210fe8dd2851152db65d6d6c2f30cba6
Georg Summer <georg.summer@gmail.com>
2014-06-16 10:21:21 +0200

    class containing some general, shared edge types. Those are not all available!

---
+++ src/main/java/org/networklibrary/core/types/EdgeTypes.java
@@ -0,0 +1,10 @@
+package org.networklibrary.core.types;
+
+public class EdgeTypes {
+
+	// predefined edgetype names!
+	public final static String TF_TARGETING = "tf_targeting";
+	public final static String MIR_TARGETING = "mir_targeting";
+	public final static String STRING = "sppi";
+	
+}
